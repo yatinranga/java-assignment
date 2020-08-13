ecc2d1735ace47b18b613d951536062a5dd92ea0
yatinranga <41532056+yatinranga@users.noreply.github.com>
2020-08-13 13:54:19 +0530

    First commit of Digilytics java assignment

---
+++ src/test/java/com/digilytics/yatin/java/assignment/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digilytics.yatin.java.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
